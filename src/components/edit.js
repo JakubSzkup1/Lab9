d3f05a41a826499fb53519de8c30c179224ff082
JakubSzkup1 <91326317+JakubSzkup1@users.noreply.github.com>
2023-12-07 19:03:21 +0000

    Update edit.js

--- src/components/edit.js
+++ src/components/edit.js
@@ -3,14 +3,17 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Edit() {
-    let {id} = useParams();
+    let {id} = useParams(); // Extracting 'id' parameter from the route using useParams hook
 
+    // State variables to manage form input values
     const [title, setTitle] = useState('');
     const [cover, setCover] = useState('');
     const [author, setAuthor] = useState('');
 
+    // Accessing the navigation function from react-router-dom
     const navigate = useNavigate();
 
+    // Fetching the book details from the server upon component mount
     useEffect(
         ()=>{
 
@@ -28,15 +31,18 @@ export default function Edit() {
         },[]
     );
 
+    // Handling form submission to update the book details
     const handleSubmit = (e)=>{
         e.preventDefault();
 
+         // Creating a book object with updated details
         const book = {
             title:title,
             cover:cover,
             author:author
         }
 
+        // Sending a PUT request to update the book on the server
         axios.put('http://localhost:4000/api/book/'+id, book)
         .then((res)=>{
             navigate('/read');
@@ -47,6 +53,8 @@ export default function Edit() {
             });
     }
     return (
+
+        //simple form for edit
         <div>
             <h2>Hello from Edit component!</h2>
             <form onSubmit={handleSubmit}>
